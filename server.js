abc3b23ebb0b158a05dfd61ce91b5b8a8f8dd6a6
DIR\erik.mohn <mohn.erik@gmail.com>
2017-04-04 15:31:36 +0200

    no message

--- server.js
+++ server.js
@@ -42,5 +42,13 @@ router.get('/parking/user/:userId', parking.getValidRequestForUser);
 
 app.use('', router);
 
-console.log('Listening on port %s...', port);
-app.listen(port);
+//console.log('Listening on port %s...', port);
+//app.listen(port);
+
+//For avoidong Heroku $PORT error
+app.get('/', function(request, response) {
+    var result = 'App is running'
+    response.send(result);
+}).listen(app.get('port'), function() {
+    console.log('App is running, server is listening on port ', app.get('port'));
+});
