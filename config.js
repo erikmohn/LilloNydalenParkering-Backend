5d45bbe68fecad0bf54660e82a58b560d6bfa454
DIR\erik.mohn <mohn.erik@gmail.com>
2017-04-04 13:26:05 +0200

    Changed MongoDB URI variable name

--- config.js
+++ config.js
@@ -1,7 +1,7 @@
 var config = {};
 config.mongodb = {};
 
-config.mongodb.url = process.env.MONGODB_URL || 'mongodb://localhost:27017';
+config.mongodb.url = process.env.MONGODB_URI || 'mongodb://localhost:27017';
 
 module.exports = config;
 
