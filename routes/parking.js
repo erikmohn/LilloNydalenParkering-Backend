cd3b29bf49ce4379645b043619f0e19f9c55e214
DIR\erik.mohn <mohn.erik@gmail.com>
2017-04-07 23:03:50 +0200

    no message

--- routes/parking.js
+++ routes/parking.js
@@ -75,7 +75,7 @@ exports.offerParking =  function(req, res) {
 								  cluster: 'eu',
 								  encrypted: true
 								});
-
+				            	console.log("Will try to push to: " + parking.requestUser._id);
 								pusher.trigger(parking.requestUser._id, 'parking-offer', {
 								  "message": "hello world"
 								});
